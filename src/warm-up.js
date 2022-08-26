d9f4a69cd046034ca6f1eba089a3da272eddc2a6
Quynh Luu <quynhtlluu@gmail.com>
2022-08-25 17:48:42 -0700

    Add answers to Variables lessons

--- src/warm-up.js
+++ src/warm-up.js
@@ -19,30 +19,42 @@ var pages = 864;
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is
-
+var pages = pages/4;
+//Using console.log(bookTitle, pages); this time should print out Anna Karenina 216
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie;
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
-
-
+var sequel = `${bookTitle}: Part II`;
+// Using console.log(sequel); should print out Anna Karenina: Part II
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
-
-
+var isFavorite = false;
+// Using console.log(isFavorite); should print out false
 
 
 // Create a new variable named "isShortStory", and use a comparison operator to
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
+var pages = 864/4;
+var isShortStory;
 
+  if (pages < 25) {
+    isShortStory = true;
+  }
+  else if (pages >= 25) {
+    isShortStory = false;
+  }
 
+console.log(isShortStory);
+//If we added console.log(isShortStory);
+//This code should print out the value of false if var pages = 864
 
 //------------------------- Conditionals -------------------------//
 
