37a2408a507df7d7450363e3e44105dd9e560cf9
Quynh Luu <quynhtlluu@gmail.com>
2022-08-27 10:15:15 -0700

    Add unshelfBook function that passes tests

--- src/shelf.js
+++ src/shelf.js
@@ -4,9 +4,19 @@ function shelfBook(book, shelf) {
   }
 }
 
+function unshelfBook(bookTitle, sciFiShelf) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title === bookTitle) {
+      sciFiShelf.splice(i, 1);
+    }
+  }
+}
+//  return shelf.splice(book, 1, 0);
+//  return shelf.pop(book);
+
 module.exports = {
    shelfBook,
-  // unshelfBook,
+   unshelfBook,
   // listTitles,
   // searchShelf
 };
