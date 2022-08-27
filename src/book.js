b90e007913a406db34e8aa1826be662ded86bc5b
Quynh Luu <quynhtlluu@gmail.com>
2022-08-26 20:23:52 -0700

    Add saveReview function that passes tests

--- src/book.js
+++ src/book.js
@@ -18,10 +18,22 @@ function buildMainCharacter(name, age, pronouns) {
 
 buildMainCharacter();
 
+var reviews = [];
+function saveReview(writtenReview, reviews) {
+  reviews.push(writtenReview);
+  for (i = 0; i < reviews.length - 1; i++) {
+    if (writtenReview === reviews[i]) {
+      reviews.pop();
+      break;
+    }
+  } console.log(reviews);
+}
+
+
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
