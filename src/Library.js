0c142f4585c7f8db82dde4c925053969567b4500
Quynh Luu <quynhtlluu@gmail.com>
2022-08-27 17:29:32 -0700

    Add checkOutBook function that passes tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,32 @@
+function createLibrary(name) {
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+}
 
+function addBook(library, bookName) {
+  var genre = bookName.genre
+  library.shelves[genre].unshift(bookName)
+}
+
+function checkoutBook(library, bookName, genre) {
+  var shelf = library.shelves[genre];
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === bookName) {
+      shelf.splice(i, 1);
+      return `You have now checked out ${bookName} from the ${library.name}`
+    }
+  }
+  return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+   createLibrary,
+   addBook,
+   checkoutBook
 };
